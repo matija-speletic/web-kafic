41837922b6ab5f54845e7e7a3a03bb1a477ce60b
matija-speletic <matijasp@elfak.rs>
2022-01-02 18:58:54 +0100

    dodata skripta

---
+++ Server/skripta.sql
@@ -0,0 +1,212 @@
+INSERT INTO [Kafic].[dbo].[Kafici]([Naziv],[Adresa],[DimenzijaX],[DimenzijaY])
+VALUES ('Index cafe','Aleksandra Medvedeva 15',5,5);
+
+INSERT INTO [Kafic].[dbo].[Kafici]([Naziv],[Adresa],[DimenzijaX],[DimenzijaY])
+VALUES ('Index cafe 2','Episkopska 20',8,8);
+
+INSERT INTO [Kafic].[dbo].[Konobari]([Ime],[Prezime],[Plata],[Nadimak],[KaficID])
+VALUES ('Marko','Miletic',50000,'Mare',1);
+
+INSERT INTO [Kafic].[dbo].[Konobari]([Ime],[Prezime],[Plata],[Nadimak],[KaficID])
+VALUES ('Kristina','Pavlovic',52000,'Kika',1);
+
+INSERT INTO [Kafic].[dbo].[Konobari]([Ime],[Prezime],[Plata],[Nadimak],[KaficID])
+VALUES ('Milos','Markovic',45000,'Somi',1);
+
+INSERT INTO [Kafic].[dbo].[Konobari]([Ime],[Prezime],[Plata],[Nadimak],[KaficID])
+VALUES ('Milica','Milivojevic',45000,'Mica',1);
+
+INSERT INTO [Kafic].[dbo].[Konobari]([Ime],[Prezime],[Plata],[Nadimak],[KaficID])
+VALUES ('Pavle','Petrovic',55000,'Paja',2);
+
+INSERT INTO [Kafic].[dbo].[Konobari]([Ime],[Prezime],[Plata],[Nadimak],[KaficID])
+VALUES ('Magdalena','Zivanovic',57000,'Lena',2);
+
+INSERT INTO [Kafic].[dbo].[Konobari]([Ime],[Prezime],[Plata],[Nadimak],[KaficID])
+VALUES ('Vladimir','Vladimirovic',51000,'Vlad',2);
+
+INSERT INTO [Kafic].[dbo].[Konobari]([Ime],[Prezime],[Plata],[Nadimak],[KaficID])
+VALUES ('Mihajlo','Mitrovski',59000,'Mixa',2);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Bakin kolac',300,250,'gr','Cokolada, narandza, brasno',2);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Topla cokolada',190,0.3,'l','',1);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Bela topla cokolada',190,0.3,'l','',1);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Coca cola',160,0.25,'l','',9);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Fanta',160,0.25,'l','',9);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Sprite',160,0.25,'l','',9);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Schweppes',160,0.25,'l','',9);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('NeXt sokovi',190,0.3,'l','Jabuka, narandza, breskva, jagoda, sumsko voce',9);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Milkshake',300,0.4,'l','Cokolada, vanila, banana',3);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Cedjena narandza',250,0.3,'l','',3);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Rosa',130,0.5,'l','',10);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Knjaz Milos',130,0.5,'l','',10);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Blurry',250,0.4,'l','Cimet, med, limun, menta',3);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Pink paradise',240,0.4,'l','Jagoda, lubenica, narandza, led',8);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Mojito',350,0.4,'l','Tonic, limun, menta, beli rum, braon secer',9);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Cuba Libre',350,0.4,'l','Coca cola, limun, rum',9);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Long Island Iced Tea',500,0.5,'l','Votka, rum, dzin, liker, limun',9);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Old Fashioned',450,0.3,'l','2 oz bourbon or rye whiskey, 2 dashes Angostura bitters, 1 sugar cube or 1 tsp sugar, Orange twist garnish',7);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Margarita',400,0.4,'l','2 oz silver tequila, 1 oz Cointreau, 1 oz lime juice, Salt for the rim',7);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Cosmopolitan',350,0.4,'l','1.5 oz citrus vodka, 1 oz Cointreau, .5 oz lime juice, .25 oz cranberry juice',7);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Negroni',350,0.4,'l','1 oz gin, 1 oz Campari, 1 oz sweet vermouth',7);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Moscow Mule',400,0.4,'l','2 oz vodka, 4 to 6 oz ginger beer, .5 oz lime juice',7);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Martini',350,0.4,'l','3 oz gin or vodka, .5 oz dry vermouth, Lemon peel or olive',7);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Whiskey Sour',350,0.4,'l','2 oz whiskey, 1 oz lemon juice, 1 tsp sugar, 1 egg white (optional)',7);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('French 75',400,0.4,'l','2 oz gin, 2 dashes simple syrup, .5 oz lemon juice, Champagne',7);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Manhattan',400,0.4,'l','2 oz rye whiskey, 1 oz sweet vermouth, 2 dashes Angostura bitters',7);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Aperol Spritz',350,0.4,'l','Equal parts Aperol, Equal parts Cinzano Prosecco, Splash of Soda',7);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Gimlet',350,0.4,'l','2 oz gin or vodka, .75 oz simple syrup, .75 oz lime juice',7);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Barbera',500,0.25,'l','Red grape variety. Most famous in the Piedmont region of NW Italy, where copious quantities are consumed with gusto. ',13);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Cabernet Franc',500,0.25,'l','Red grape variety and member of the Cabernet family. Bright raspberry fruit and occasionally leafy tobacco and green herb flavors.',13);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Cabernet Sauvignon',500,0.25,'l','Red grape variety. Widely planted with Bordeaux as its historical reference point. Often blended with Merlot and Cabernet Franc.',13);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Carignan',500,0.25,'l','Red grape variety. The most common grape variety in France and widely planted in the Midi. Carignan tends to be low in acidity, extract and tannin making it useful for blending.',13);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Charbono',500,0.25,'l','Red grape variety. Obscure at best and probably better known in California than anywhere else, although its origins point to southern France.',13);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Chardonnay',500,0.25,'l',' White grape variety. The white wine darling of the wine world. Widely planted and fairly successful in many areas of the world.',13);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Dolcetto',500,0.25,'l',' Red grape variety. In the dialect of Piedmont, northern Italy, Dolcetto means “little sweet one.” This is not because the wines are sweet but because the grapes ripen relatively easily.',13);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Gamay',500,0.25,'l','Red grape variety. Although grown in the Loire Valley, the Ardeche, Switzerland, and California (alias Napa Gamay), Gamay is truly at its gulpable best',13);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Grenache',500,0.25,'l','Red grape variety. Widely planted in southern France and in Spain and a frequent component in Rhone-style blends produced in other parts of the world.',13);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Malbec',500,0.25,'l','a red grape variety. In the Bordeaux region of France, in California, and in other parts of the world, the Malbec grape variety is usually used as a blending component ',13);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Malvasia',500,0.25,'l','a white grape variety. Malvasia is one of the most ancient grape varieties, and is thought by some to be a member of the aromatic Muscat family',13);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Marsanne',500,0.25,'l','a white grape variety. Marsanne shares the spotlight with Roussanne in Frances Rhone Valley. It is also widely planted in Australia but uncommon elsewhere.',13);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Merlot',500,0.25,'l','a red grape variety. It is most famous in wines such as Chateau Petrus from the Pomerol region of Bordeaux',13);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Mourvedre',500,0.25,'l','a red grape variety. Also known as Mataro, Mourvedre is widely planted in southern France, Spain and California.',13);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Grilovana piletina',700,500,'gr','',4);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Pesto pasta sa piletinom',650,500,'gr','',4);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Carbonara',670,500,'gr','',4);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Burito',450,500,'gr','',4);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Pulled pork burger',700,500,'gr','',4);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Cheeseburger',650,500,'gr','',4);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Chicken burger',650,500,'gr','',4);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Piletina u kesten sosu',800,500,'gr','',4);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Rizoto sa morskim plodovima',710,500,'gr','',4);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Sushi',900,8,'kom','dragon, california',4);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Grilovani oktopod',1200,400,'gr','',4);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Sabljarka',1000,400,'gr','',4);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Pohovane tikvice',400,300,'gr','',4);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Srpski obrok',800,650,'gr','',4);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Pomfrit',200,300,'gr','',6);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Domaci krompir',300,300,'gr','',6);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Pohovani kackavalj',350,2,'kom','',6);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Patatas bravas',330,300,'gr','',6);
+
+INSERT INTO [Kafic].[dbo].[Proizvodi]([Naziv],[Cena],[Kolicina],[Jedinica],[Opis],[KategorijaID])
+VALUES ('Onion rings',330,10,'kom','',6);
